af12477377a989731dd15440626fe08b480bafb2
Sejal Khanzode <khanzodesejal@gmail.com>
2023-10-15 09:50:59 +0530

    Home page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-
-import './App.css';
+import "./App.css";
+import {Route, Routes } from "react-router-dom";
+import Home from "./pages/Home"
+import Navbar from "./components/common/Navbar"
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+   <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+   <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+   </div>
   );
 }
 
