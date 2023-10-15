1456673a78c2f595c95e9b89dc5df376dc7d89ce
Sejal Khanzode <khanzodesejal@gmail.com>
2023-10-15 19:07:26 +0530

    models done

---
+++ server/models/Course.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+    courseName:{
+        type:String,
+        trim:true,
+    },
+    courseDescription:{
+        type:String,
+        trim:true,
+    },
+    instructor:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    whatYouWillLearn:{
+        type:String,
+    },
+    courseContent:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Section",
+        required:true
+    }],
+    ratingAndReviews:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"RatingAndReview",
+        required:true
+    }],
+    price:{
+        type:Number,
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        required:true
+    },
+    tag:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Tag",
+        required:true
+    },
+    studentsEnrolled:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    }]
+})
+
+module.exports = mongoose.model("CourseSchema", courseSchema);
