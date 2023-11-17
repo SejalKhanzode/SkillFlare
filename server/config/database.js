72f2d8ac9b9d84d80c7cf110e793aa91fa879823
Sejal Khanzode <khanzodesejal@gmail.com>
2023-11-17 19:40:06 +0530

    backend done

--- server/config/database.js
+++ server/config/database.js
@@ -1,17 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-exports.connect=()=>{
-    mongoose.connect (process.env.MONGODB_URL,{
-        useNewUrlParser:true,
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
         useUnifiedTopology:true,
     })
-    .then(() =>{
-        console.log("DB Connection Successfully")
-    })
-    .catch((error)=>{
+    .then(() => console.log("DB Connected Successfully"))
+    .catch( (error) => {
         console.log("DB Connection Failed");
         console.error(error);
-        process.getMaxListeners(1);
-    })
-}
+        process.exit(1);
+    } )
+};
